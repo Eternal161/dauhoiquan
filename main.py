5b11f9fc96652d7f30999b990e68852b220583c5
Eternal161 <daulam1601@gmail.com>
2026-05-15 15:57:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from playwright_stealth import Stealth
 TARGET_SITE   = "https://sv2.hoiquan3.live/lich-thi-dau/bong-da"
 BASE_URL      = "https://sv2.hoiquan3.live"
 FILE_PATH     = "hoiquan.json"
-LIMIT_MATCHES = 20 # Bạn có thể chỉnh số lượng trận muốn quét ở đây
+LIMIT_MATCHES = 10 # Bạn có thể chỉnh số lượng trận muốn quét ở đây
 
 VN_TZ = datetime.timezone(datetime.timedelta(hours=7))
 
