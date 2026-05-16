ad74f15f215cf99bd4918d72fd71a7a4204d6292
Eternal161 <daulam1601@gmail.com>
2026-05-16 22:24:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from playwright_stealth import Stealth
 TARGET_SITE   = "https://sv2.hoiquan3.live/lich-thi-dau/bong-da"
 BASE_URL      = "https://sv2.hoiquan3.live"
 FILE_PATH     = "hoiquan.json"
-LIMIT_MATCHES = 10 
+LIMIT_MATCHES = 15 
 
 VN_TZ = datetime.timezone(datetime.timedelta(hours=7))
 GITHUB_TOKEN = os.getenv("GH_TOKEN")
